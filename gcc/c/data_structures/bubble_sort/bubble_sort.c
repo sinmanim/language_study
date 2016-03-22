8bb2ca25ecc2092a04428b7733fbccacaf447ea9
minseop <seop@localhost.localdomain>
2016-03-22 16:39:55 +0900

    2016-03-22 test.c commit

---
+++ gcc/c/data_structures/bubble_sort/bubble_sort.c
@@ -0,0 +1,83 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 10
+
+void bubble_sort(int array[], int length);
+
+void print(int array[]);
+
+int main()
+{
+	int i, buf[10];
+	srand(time(NULL));	
+
+	printf("size of buf -> %d\n", sizeof(buf));
+
+	printf("bubble sort start\n\n");
+
+	for(i=0; i<10; i++)
+	{
+		buf[i] = (long) rand() * 100 / RAND_MAX;
+		printf("%3d ", buf[i]);
+	}
+
+	printf("\n");
+
+	bubble_sort(buf, 10);
+
+	print(buf);
+
+	return 0;
+}
+
+void bubble_sort(int array[], int length)
+{
+	int i, j;
+	int next;
+	
+	for(i = length-1; i>0; i--)
+	{	
+		for(j=0; j<i; j++)
+		{
+			if(array[j] > array[j+1]){
+				next = array[j];
+				array[j] = array[j+1];
+				array[j+1] = next;
+			}
+		}
+	}
+}
+
+void print(int array[])
+{
+	int i;
+
+	for(i=0; i<MAX; i++)
+	{
+		printf("%3d ", array[i]);
+	}
+	printf("\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- gcc/c/temp/test.c
+++ gcc/c/temp/test.c
@@ -1,10 +1,64 @@
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void test(int** array, int size);
 
 void main(void)
 {
-	puts("test");
-
+	int buf[10] = {1,2,3,4,5,6,7,8,};
+	int *bufC = &buf;
+	test(&bufC, 10);
 }
+
+void test(int** array, int size)
+{
+	int i;
+	//printf("%d ", (*array)[3]);
+
+	int *tt = (int*) malloc(size*sizeof(int));
+	//int tt[10]={};
+
+	memcpy(tt, &(*array), size*sizeof(int));
+
+	for(i=0; i<10; i++)
+	{	
+		printf("%d ", tt[i]);
+	}
+
+	free(tt);
+}
+
+
+
+
+
+
 
